ec15e98c39d9162e2f0c8d26636cb0163f21a5cf
ayal <ayalg@wix.com>
2020-03-28 21:32:55 +0300

    better

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import "./styles.css";
 
 console.log(window.location);
 const rand = x => Math.floor(Math.random() * x);
+let first;
+let second;
 let cards = [];
+
 if (window.location.hash) {
-  cards = JSON.parse(decodeURIComponent(window.location.hash.split("#")[1]));
+  try {
+      const obj = JSON.parse(decodeURIComponent(window.location.hash.split("#")[1]));
+      cards = obj.cards
+      first = obj.first;
+      second = obj.second;
+      console.log('from hash', cards, first, second);
+  }
+  catch (ex) {
+    
+  }
+
+  if (!first || !cards) {
+    window.history.pushState(null, null, window.location.href.split('#')[0]);
+    setTimeout(()=>{
+      window.location.reload()
+    })
+  }
+
 } else {
-  const first = rand(2) === 0 ? "red" : "blue";
-  const second = first === "red" ? "blue" : "red";
+  first = rand(2) === 0 ? "r" : "b";
+  second = first === "r" ? "b" : "r";
   console.log("first / second", first, second);
 
   for (let i = 0; i < 8; ) {
@@ -30,17 +50,17 @@ if (window.location.hash) {
   for (let i = 0; i < 4; ) {
     let x = rand(20);
     if (!cards[x]) {
-      cards[x] = "yellow";
+      cards[x] = "y";
       i++;
     }
   }
 
   for (let i = 0; i < 20; i++) {
     if (!cards[i]) {
-      cards[i] = "black";
+      cards[i] = "x";
     }
   }
-  window.location.hash = JSON.stringify(cards);
+  window.location.hash = JSON.stringify({cards, first, second});
 }
 
 console.log(cards);
@@ -50,8 +70,20 @@ const Cell = props => {
 };
 
 export default function App() {
+  if (!first || !cards) {
+    return null;
+  }
   const cardels = cards.map((c, i) => {
     return <Cell key={c + "-" + i} color={c} index={i} />;
   });
-  return <div className="App">{cardels}</div>;
+  return (<div>
+      <div className="App">
+        <div className="board">{cardels}</div>
+      <div>{`first to play: ${first}`}</div>
+      <button onClick={x=>{
+        window.history.pushState(null, null, window.location.href.split('#')[0]);
+        window.location.reload()
+      }}>new</button>
+    </div>
+    </div>);
 }
