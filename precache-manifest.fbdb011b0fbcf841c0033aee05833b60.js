59dc61114f5138bc7629698df4492ebed1ce847c
ayal <ayalg@wix.com>
2020-03-28 21:01:03 +0300

    Updates

---
+++ precache-manifest.fbdb011b0fbcf841c0033aee05833b60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e883bd78ceb3e2dcc6f3e9797530460",
+    "url": "/condamez/index.html"
+  },
+  {
+    "revision": "c541216613c882b49415",
+    "url": "/condamez/static/css/main.e2dbab10.chunk.css"
+  },
+  {
+    "revision": "0e9bc0986a32834066b5",
+    "url": "/condamez/static/js/2.e23f0133.chunk.js"
+  },
+  {
+    "revision": "c541216613c882b49415",
+    "url": "/condamez/static/js/main.8b8b12c7.chunk.js"
+  },
+  {
+    "revision": "c2eb2066c9aef6742b41",
+    "url": "/condamez/static/js/runtime~main.a37810b9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6bf99f92d222dc0922a5dc4082532dc.js"
+  "/condamez/precache-manifest.fbdb011b0fbcf841c0033aee05833b60.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/condamez/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
